1c2c1c58a8baed693e358bc5db2db0612ca167fa
ArdenKirk <Arden@talesofcharlie.com>
2025-09-13 17:06:51 -0400

    add initial tests and update dependencies

---
+++ apps/web/src/__tests__/placeholder.test.ts
@@ -0,0 +1,7 @@
+import { describe, expect, test } from "vitest";
+
+describe("Web Tests", () => {
+  test("passes", () => {
+    expect(true).toBe(true);
+  });
+});
---
+++ apps/worker/__tests__/placeholder.test.ts
@@ -0,0 +1,7 @@
+import { describe, expect, test } from "vitest";
+
+describe("Worker Tests", () => {
+  test("passes", () => {
+    expect(true).toBe(true);
+  });
+});
